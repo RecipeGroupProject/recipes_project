7ec4dd84ba5dabcf74d4831ff66c3cb7b04f0051
Landen Wasserstrom <Landen.Wasserstrom@gmail.com>
2017-09-05 15:36:24 -0700

    updated update.js and update.ejs

--- assets/js/update.js
+++ assets/js/update.js
@@ -44,7 +44,87 @@
 
    $(function(){
 
-    //code goes here
+     $("#updateRecipeForm :input").prop("disabled", true);
+
+     $('#recipe_id').selectpicker({
+        style: 'btn-info',
+        size: 8
+     });
+
+     let currentRecipe
+
+    $("#recipe_id").change(function () {
+
+
+          currentRecipe = $(this).val()
+
+           $.get("http://localhost:1337/hero/" + currentRecipe, function(recipe){
+
+
+             $.each(recipe, function(key, val){
+
+                 let el = $('[name="'+key+'"]');
+                 let type = el.attr('type');
+
+                 switch(type){
+                     case 'checkbox':
+                         el.attr('checked', 'checked');
+                         break;
+                     case 'radio':
+                         el.filter('[value="'+val+'"]').attr('checked', 'checked');
+                         break;
+                     default:
+                         el.val(val);
+                 }
+              });
+           })
+           $("#updateRecipeForm :input").prop("disabled", false);
+         })
+
+         $("#updateRecipeButton").click(function(e){
+
+           e.preventDefault()
+
+            $.ajax({
+            url: "http://localhost:1337/recipes/" + currentRecipe,
+            data: $("#updateRecipeForm").serialize(),
+            type: 'PUT',
+            success: function(result) {
+                alert("Successfully Changed")
+            }
+            });
+         $("#updateRecipeForm :input").prop("disabled", true);
+         })
+         $("#updateRecipeForm").validate({
+            rules: {
+         recipe_name: {
+           required: true,
+         },
+         recipe_des: {
+           required: true
+         },
+         perp_time: {
+           required: true
+         }
+       },
+       messages: {
+         recipe_name: {
+           required: "We require a recipe name"
+         },
+         recipe_des: {
+           required: "We require a recipe description"
+         },
+         prep_time: {
+           required: "We require a preparation time"
+         }
+      },
+        highlight: function (element) {
+            $(element).parent().addClass('error')
+        },
+        unhighlight: function (element) {
+            $(element).parent().removeClass('error')
+        }
+    });
 
    })
 
